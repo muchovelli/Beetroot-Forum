84431843d6b330a573e09dccd4b6af235f00e111
muchovelli <adamdanda@protonmail.com>
2023-04-29 23:57:56 +0200

    Initial commit

---
+++ src/main/java/com/beetrootforum/beetrootforum/BeetrootForumApplication.java
@@ -0,0 +1,11 @@
+package com.beetrootforum.beetrootforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeetrootForumApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BeetrootForumApplication.class, args);
+    }
+}
